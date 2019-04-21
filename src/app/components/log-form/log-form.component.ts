6611767d74083a0c75e579ce3e1f484a761340dc
aabedkashwa <kashwa80@gmail.com>
2019-04-21 14:12:41 +0200

    to be cont'd. Observable component

--- src/app/components/log-form/log-form.component.ts
+++ src/app/components/log-form/log-form.component.ts
@@ -13,20 +13,46 @@ export class LogFormComponent implements OnInit {
   text: string;
   date: any;
 
+  isNew: boolean = true;
+
   constructor(private logService: LogService) { }
 
   ngOnInit() {
     // subscribe to the event (selected log observable). to put in the form.
     this.logService.selectedLog.subscribe(log => {
-      //console.log(log) // initially it will log it with null values, (initial value).
+      //console.log(log) # initially it will log it with null values, (initial value).
 
       // if something is clicked.
       if (log.id != null) {
+        this.isNew = false;
         this.id = log.id;
         this.text = log.text;
         this.date = log.date;
       }
-    })
+    });
+
   }
 
+  onSubmit() {
+    // check if new log
+    if (this.isNew) {
+      const newLog = {
+        id: this.generateID(),
+        text: this.text,
+        date: new Date()
+      }
+      // Add log
+      this.logService.addLog(newLog);
+    } else {
+      // TODO: update log.
+    }
+  }
+
+  // create UUID for us
+  generateID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
 }
